7dcdf2048d91ef2267d1db32c1029327cabe7887
Avani Gokhale <36026630+avani112@users.noreply.github.com>
2019-07-31 19:31:51 +0530

    Create ListToArray.java

---
+++ ListToArray.java
@@ -0,0 +1,8 @@
+String[] values = new String[]{ "one", "two", "three" };
+List list = Arrays.asList(values);
+List<String> list = new ArrayList<>();
+list.add("element 1");
+list.add("element 2");
+list.add("element 3");
+list.add("element 3");
+String[] objects1=list.toArray(new String[0]);
